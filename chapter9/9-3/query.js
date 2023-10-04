eaaef20abee5c4022f187fc6a29f572fefbda4d5
Tony Lee <tony@treasurer.co.kr>
2023-10-04 13:17:10 +0900

    9-3 파생 변수를 질의함수로 바꾸기: step1~2. 갱신되는 변수를 찾고 변수값을 계산하는 함수 만들기

--- chapter9/9-3/query.js
+++ chapter9/9-3/query.js
@@ -1,15 +1,20 @@
 // 파생 변수를 질의 함수로 바꾸기
 class ProductionPlan {
-  constructor(production, adjustment) {
+  constructor(production, adjustments) {
     this._production = production;
-    this._adjustment = adjustment;
+    this._adjustments = adjustments;
   }
 
   get production() {
     return this._production;
   }
+
+  get calculatedProduction() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+  }
+
   applyAdjustment(anAdjustment) {
-    this._adjustment.push(anAdjustment);
+    this._adjustments.push(anAdjustment);
     this._production += anAdjustment.amount;
   }
 }
