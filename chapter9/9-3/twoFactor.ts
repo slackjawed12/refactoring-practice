cf40364f39b25be2be47feed96a22df1799dd60e
Tony Lee <tony@treasurer.co.kr>
2023-10-05 07:56:52 +0900

    9-3 파생 변수를 질의함수로 바꾸기2: 파생변수에 영향주는 요소가 하나일때와 차이점

--- chapter9/9-3/twoFactor.ts
+++ chapter9/9-3/twoFactor.ts
@@ -12,12 +12,18 @@ class ProductionPlan {
     this._adjustment = [];
   }
 
+  // 앞선 예시와 같이 assert를 하면, production 초기값이 0이 아닐 때 assertion이 실패한다.
   get production() {
+    console.assert(this._production === this.calculatedProduction);
     return this._production;
   }
 
   applyAdjustment(anAdjustment: Adjustment) {
     this._adjustment.push(anAdjustment);
     this._production += anAdjustment.amount;
+  }
+
+  get calculatedProduction() {
+    return this._adjustment.reduce((sum, a) => sum + a.amount, 0);
   }
 }
