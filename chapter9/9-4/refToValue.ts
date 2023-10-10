878e3255a505b2a771135f00c0622e2e6cc54e00
Tony Lee <tony@treasurer.co.kr>
2023-10-10 13:15:58 +0900

    9-4 참조를 값으로 바꾸기 : init

---
+++ chapter9/9-4/refToValue.ts
@@ -0,0 +1,42 @@
+class Person {
+  _telephoneNumber;
+  constructor() {
+    this._telephoneNumber = new this._telephoneNumber();
+  }
+
+  get officeAreaCode() {
+    return this._telephoneNumber.areaCode;
+  }
+
+  set officeAreaCode(arg) {
+    this._telephoneNumber.areaCode = arg;
+  }
+
+  get officeNumber() {
+    return this._telephoneNumber.number;
+  }
+
+  set officeNumber(arg) {
+    this._telephoneNumber.number = arg;
+  }
+}
+
+class TelephoneNumber {
+  _areaCode: string;
+  _number: string;
+  get areaCode() {
+    return this._areaCode;
+  }
+
+  set areaCode(arg) {
+    this._areaCode = arg;
+  }
+
+  get number() {
+    return this._number;
+  }
+
+  set number(arg) {
+    this._number = arg;
+  }
+}
