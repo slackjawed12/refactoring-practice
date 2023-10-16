cd7e6089c7aa79099b0a97c83026fada73acc070
Tony Lee <tony@treasurer.co.kr>
2023-10-16 13:27:36 +0900

    10-1 조건문 분해하기 : step 1 - 조건식을 별도 함수로 추출

--- chapter10/decomposeConditional.ts
+++ chapter10/decomposeConditional.ts
@@ -1,6 +1,10 @@
 function calculateFee(aDate, plan, quantity) {
   let charge: number;
-  if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+  function summer() {
+    return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+  }
+
+  if (summer()) {
     charge = quantity * plan.summerRate;
   } else {
     charge = quantity * plan.regularRate + plan.regularServiceCharge;
