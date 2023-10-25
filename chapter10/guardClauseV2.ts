07bc20cc4e13e0b4e073853d265c5eeb3ecd1c96
Tony Lee <tony@treasurer.co.kr>
2023-10-25 13:37:33 +0900

    10-3 중첩 조건문을 보호 구문으로 바꾸기: step4 조건식 or로 통합하기

--- chapter10/guardClauseV2.ts
+++ chapter10/guardClauseV2.ts
@@ -1,8 +1,11 @@
 // 조건식을 반대로 만들어서 리팩터링
 function adjustedCapital(anInstrument) {
   let result = 0;
-  if (anInstrument.capital <= 0) return result;
-  if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0)
+  if (
+    anInstrument.capital <= 0 ||
+    anInstrument.interestRate <= 0 ||
+    anInstrument.duration <= 0
+  )
     return result;
 
   result =
