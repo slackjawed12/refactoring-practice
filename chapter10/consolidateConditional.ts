623380bbc20a27f5097af5894dbbb6502cb38346
Tony Lee <tony@treasurer.co.kr>
2023-10-25 13:11:46 +0900

    10-2 조건식 통합하기: step 1 두 개를 선택해서 or 연산자로 통합

--- chapter10/consolidateConditional.ts
+++ chapter10/consolidateConditional.ts
@@ -5,8 +5,7 @@ class Employee {
 }
 
 function disabilityAmount(anEmployee: Employee) {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthsDisabled > 12) return 0;
+  if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12) return 0;
   if (anEmployee.isPartTime) return 0;
 
   // 장애 수당 계산
