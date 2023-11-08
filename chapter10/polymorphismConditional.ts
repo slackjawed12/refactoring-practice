72cd3d4a7d5ee2df1c79672b05ac37936c09d0b7
Tony Lee <tony@treasurer.co.kr>
2023-11-08 13:36:48 +0900

    10-3 조건부 로직을 다형성으로 바꾸기 : step 5 - 나머지 서브클래스 메서드 오버라이드 및 슈퍼클래스 메서드는 기본동작으로 변경

--- chapter10/polymorphismConditional.ts
+++ chapter10/polymorphismConditional.ts
@@ -37,16 +37,7 @@ class Bird {
   }
 
   get plumage() {
-    switch (this.type) {
-      case "유럽 제비":
-        throw "오류 발생";
-      case "아프리카 제비":
-        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-      case "노르웨이 파랑 앵무":
-        return this.voltage > 100 ? "그을렸다" : "예쁘다";
-      default:
-        return "알 수 없다";
-    }
+    return "알 수 없다";
   }
 
   get airSpeedVelocity() {
@@ -68,7 +59,15 @@ class EuropeanSwallow extends Bird {
     return "보통이다" as const;
   }
 }
-class AfricanSwallow extends Bird {}
-class NorwegianBlueParrot extends Bird {}
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+  }
+}
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return this.voltage > 100 ? "그을렸다" : "예쁘다";
+  }
+}
 
 export {};
