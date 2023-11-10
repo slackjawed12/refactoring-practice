8ff1e3be36f54194a127cc755206ba4d9c6370b6
Tony Lee <tony@treasurer.co.kr>
2023-11-10 13:28:10 +0900

    10-4 조건부 로직을 다형성으로 바꾸기: 서브클래스 메서드 오버라이드 2 - 조건부 블록 함수 추출

--- chapter10/polymorphismConditionalV2.ts
+++ chapter10/polymorphismConditionalV2.ts
@@ -145,6 +145,20 @@ class Rating {
     return result;
   }
 
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    if (this.voyage.zone === "중국" && hasChina(this.history)) {
+      result += 3;
+      if (this.history.length > 10) result += 1;
+      if (this.history.length > 12) result += 1;
+      if (this.voyage.length > 18) result -= 1;
+    } else {
+      if (this.history.length > 8) result += 1;
+      if (this.voyage.length > 14) result -= 1;
+    }
+    return result;
+  }
+
   get hasChinaHistory() {
     return this.history.some((v) => "중국" === v.zone);
   }
