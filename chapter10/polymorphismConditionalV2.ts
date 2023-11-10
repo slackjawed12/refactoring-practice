6ff4d08b89257676e4b1a30bec40ae08b8c0046d
Tony Lee <tony@treasurer.co.kr>
2023-11-10 13:19:38 +0900

    10-4 조건부 로직을 다형성으로 바꾸기: Rating 클래스 생성 (함수를 클래스로 추출)

--- chapter10/polymorphismConditionalV2.ts
+++ chapter10/polymorphismConditionalV2.ts
@@ -90,5 +90,76 @@ const historyList = [
   },
 ];
 
+type Voyage = {
+  zone: string;
+  length: number;
+};
+type History = { zone: string; profit: number }[];
+
+class Rating {
+  voyage: Voyage;
+  history: History;
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
+  }
+
+  get value() {
+    const vpf = voyageProfitFactor(this.voyage, history);
+    const vr = voyageRisk(this.voyage);
+    const chr = captainHistoryRisk(this.voyage, history);
+    if (vpf * 3 > vr + chr * 2) return "A";
+
+    return "B";
+  }
+
+  get voyageRisk() {
+    let result = 1;
+    if (this.voyage.length > 4) {
+      result += 2;
+    }
+    if (this.voyage.length > 8) {
+      result += this.voyage.length - 8;
+    }
+    if (["중국", "동인도"].includes(this.voyage.zone)) {
+      result += 4;
+    }
+    return Math.max(result, 0);
+  }
+
+  get captainHistoryRisk() {
+    let result = 1;
+    if (this.history.length < 5) {
+      result += 4;
+    }
+    result += this.history.filter((v) => v.profit < 0).length;
+    if (this.voyage.zone === "중국" && hasChina(this.history)) {
+      result -= 2;
+    }
+    return Math.max(result, 0);
+  }
+
+  get voyageProfitFactor() {
+    let result = 2;
+    if (this.voyage.zone === "중국") result += 1;
+    if (this.voyage.zone === "동인도") result += 1;
+    if (this.voyage.zone === "중국" && hasChina(this.history)) {
+      result += 3;
+      if (this.history.length > 10) result += 1;
+      if (this.history.length > 12) result += 1;
+      if (this.voyage.length > 18) result -= 1;
+    } else {
+      if (this.history.length > 8) result += 1;
+      if (this.voyage.length > 14) result -= 1;
+    }
+    return result;
+  }
+
+  get hasChinaHistory() {
+    return this.history.some((v) => "중국" === v.zone);
+  }
+}
 const myRating = rating(voyageInfo, historyList);
 console.log(myRating);
+
+export default {};
