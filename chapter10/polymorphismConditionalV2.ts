79be0d5d418bb91351cfa3a7d447ed52e23af5f9
Tony Lee <tony@treasurer.co.kr>
2023-11-14 13:24:55 +0900

    10-4 조건부 로직을 다형성으로 바꾸기: 기존 함수 로직 삭제

--- chapter10/polymorphismConditionalV2.ts
+++ chapter10/polymorphismConditionalV2.ts
@@ -5,84 +5,6 @@ function rating(voyage, history) {
   return createRating(voyage, history).value;
 }
 
-/**
- * 항해 경로 위험 요소
- */
-function voyageRisk(voyage) {
-  let result = 1;
-  if (voyage.length > 4) {
-    result += 2;
-  }
-  if (voyage.length > 8) {
-    result += voyage.length - 8;
-  }
-  if (["중국", "동인도"].includes(voyage.zone)) {
-    result += 4;
-  }
-  return Math.max(result, 0);
-}
-
-/**
- * 선장의 항해 이력 위험요소
- */
-function captainHistoryRisk(voyage, history) {
-  let result = 1;
-  if (history.length < 5) {
-    result += 4;
-  }
-  result += history.filter((v) => v.profit < 0).length;
-  if (voyage.zone === "중국" && hasChina(history)) {
-    result -= 2;
-  }
-  return Math.max(result, 0);
-}
-
-/**
- * 중국 경유 여부
- */
-function hasChina(history) {
-  return history.some((v) => "중국" === v.zone);
-}
-
-/**
- * 수익 요인
- */
-function voyageProfitFactor(voyage, history) {
-  let result = 2;
-  if (voyage.zone === "중국") result += 1;
-  if (voyage.zone === "동인도") result += 1;
-  if (voyage.zone === "중국" && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) result += 1;
-    if (history.length > 12) result += 1;
-    if (voyage.length > 18) result -= 1;
-  } else {
-    if (history.length > 8) result += 1;
-    if (voyage.length > 14) result -= 1;
-  }
-  return result;
-}
-
-const voyageInfo = { zone: "서인도", length: 10 };
-const historyList = [
-  {
-    zone: "동인도",
-    profit: 5,
-  },
-  {
-    zone: "서인도",
-    profit: 15,
-  },
-  {
-    zone: "중국",
-    profit: -2,
-  },
-  {
-    zone: "서아프리카",
-    profvit: 7,
-  },
-];
-
 type Voyage = {
   zone: string;
   length: number;
@@ -98,9 +20,9 @@ class Rating {
   }
 
   get value() {
-    const vpf = voyageProfitFactor(this.voyage, history);
-    const vr = voyageRisk(this.voyage);
-    const chr = captainHistoryRisk(this.voyage, history);
+    const vpf = this.voyageProfitFactor;
+    const vr = this.voyageRisk;
+    const chr = this.captainHistoryRisk;
     if (vpf * 3 > vr + chr * 2) return "A";
 
     return "B";
@@ -177,6 +99,26 @@ function createRating(voyage, history) {
     return new ExperiencedChinaRating(voyage, history);
   } else return new Rating(voyage, history);
 }
+
+const voyageInfo = { zone: "서인도", length: 10 };
+const historyList = [
+  {
+    zone: "동인도",
+    profit: 5,
+  },
+  {
+    zone: "서인도",
+    profit: 15,
+  },
+  {
+    zone: "중국",
+    profit: -2,
+  },
+  {
+    zone: "서아프리카",
+    profvit: 7,
+  },
+];
 
 const myRating = rating(voyageInfo, historyList);
 console.log(myRating);
