222af2311ffbb636a26190199f813ddea32a025c
Tony Lee <tony@treasurer.co.kr>
2023-11-15 13:15:28 +0900

    10-5 특이케이스 추가하기 : init

---
+++ chapter10/specialCase.ts
@@ -0,0 +1,36 @@
+class Site {
+  _customer: Customer;
+  constructor(customer) {
+    this._customer = customer;
+  }
+  get customer() {
+    return this._customer;
+  }
+}
+
+class Customer {
+  _name;
+  _billingPlan;
+  _paymentHistory;
+  constructor(name, billingPlan, paymentHistory) {
+    this._name = name;
+    this._billingPlan = billingPlan;
+    this._paymentHistory = paymentHistory;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get billingPlan() {
+    return this._billingPlan;
+  }
+
+  set billingPlan(arg) {}
+
+  get paymentHistory() {
+    return this._paymentHistory;
+  }
+}
+
+export default {};
