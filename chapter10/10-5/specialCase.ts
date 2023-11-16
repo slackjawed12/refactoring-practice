37208e5dbc5f39e0bdedf0f0b8a3c9a63d0c8056
Tony Lee <tony@treasurer.co.kr>
2023-11-16 13:17:00 +0900

    10-5 특이케이스 추가하기 : step7 - 특이케이스가 객체를 반환하는 경우

--- chapter10/10-5/specialCase.ts
+++ chapter10/10-5/specialCase.ts
@@ -41,6 +41,7 @@ export class Customer {
 
 /**
  * javascript 동적 타이핑 특성 상 서브클래스로 만들지 않는 것이 나음
+ * 특이 케이스 객체는 값 객체로, 불변이다.
  */
 export class UnkownCustomer {
   get isUnknown() {
@@ -54,6 +55,16 @@ export class UnkownCustomer {
     return registry.billingPlans.basic;
   }
   set billingPlan(arg) {} // 무시
+
+  get paymentHistory() {
+    return new NullPaymentHistory();
+  }
+}
+
+export class NullPaymentHistory {
+  get weeksDeliquentInLastYear() {
+    return 0;
+  }
 }
 
 export function isUnknown(arg) {
