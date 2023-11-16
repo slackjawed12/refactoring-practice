57a26e87ca498cc26f0fbd88892bef6dc0b17252
Tony Lee <tony@treasurer.co.kr>
2023-11-16 13:22:26 +0900

    10-5 특이케이스 추가하기 : step7 - 전역함수 호출을 클래스 메서드 호출로 수정

--- chapter10/10-5/strangeClient.ts
+++ chapter10/10-5/strangeClient.ts
@@ -1,7 +1,5 @@
-import { isUnknown } from "./specialCase";
-
 // 디폴트 값으로 "거주자"가 아닌 "미확인 거주자"를 사용하는 클라이언트
 export const strangeClient = (aCustomer) => {
-  const name = !isUnknown(aCustomer) ? aCustomer.name : "미확인 거주자";
+  const name = aCustomer.isUnknown ? "미확인 거주자" : aCustomer.name;
   return name;
 };
