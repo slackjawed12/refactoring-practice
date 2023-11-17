1ac44ca45964011dcfba02ec59c57129bd20705a
Tony Lee <tony@treasurer.co.kr>
2023-11-17 13:35:50 +0900

    10-7 제어 플래그를 탈출문으로 바꾸기 : step1. 함수 추출

--- chapter10/10-7/flagToBreak.ts
+++ chapter10/10-7/flagToBreak.ts
@@ -3,16 +3,20 @@ const sendAlert = () => {
   console.log("악당 나타남");
 };
 
-let found = false;
-for (const p of people) {
-  if (!found) {
-    if (p === "조커") {
-      sendAlert();
-      found = true;
-    }
-    if (p === "사루만") {
-      sendAlert();
-      found = true;
+checkForMiscreants(people);
+
+function checkForMiscreants(people) {
+  let found = false;
+  for (const p of people) {
+    if (!found) {
+      if (p === "조커") {
+        sendAlert();
+        found = true;
+      }
+      if (p === "사루만") {
+        sendAlert();
+        found = true;
+      }
     }
   }
 }
