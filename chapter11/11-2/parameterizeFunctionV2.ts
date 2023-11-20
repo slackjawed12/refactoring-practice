4f33945c444201759d49601223fef80869e388ec
Tony Lee <tony@treasurer.co.kr>
2023-11-20 13:29:00 +0900

    11-2 함수 매개변수화하기 : step3-5. 함수에서 사용한 리터럴을 매개변수로 대체

--- chapter11/11-2/parameterizeFunctionV2.ts
+++ chapter11/11-2/parameterizeFunctionV2.ts
@@ -29,5 +29,5 @@ function topBand(usage: number) {
 }
 
 function withinBand(usage: number, bottom: number, top: number) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+  return usage > bottom ? Math.min(usage, 200) - bottom : 0;
 }
