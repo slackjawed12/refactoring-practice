813ca1409068e2d3a6ba6270fe4b40ed685c1791
Tony Lee <tony@treasurer.co.kr>
2023-11-20 13:19:09 +0900

    11-2 함수 매개변수화하기 : 간단한 예시

--- chapter11/11-2/parameterizeFunction.ts
+++ chapter11/11-2/parameterizeFunction.ts
@@ -5,3 +5,7 @@ function tenPercentRaise(aPerson) {
 function fivePercentRaise(aPerson) {
   aPerson.salary = aPerson.salary.multiply(1.05);
 }
+
+function raise(aPerson, factor) {
+  aPerson.salary = aPerson.salary.multiply(1 + factor);
+}
