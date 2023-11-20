febad882f6978e54f6f16090f47b6833afcc5aa8
Tony Lee <tony@treasurer.co.kr>
2023-11-20 13:28:02 +0900

    11-2 함수 매개변수화하기 : step1,2. 중간 범위 함수를 선택하고, 매개변수화된 함수 작성

--- chapter11/11-2/parameterizeFunctionV2.ts
+++ chapter11/11-2/parameterizeFunctionV2.ts
@@ -6,7 +6,9 @@ function baseCharge(usage: number) {
     return usd(0);
   }
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    bottomBand(usage) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    topBand(usage) * 0.07;
   return usd(amount);
 }
 
@@ -24,4 +26,8 @@ function middleBand(usage: number) {
 
 function topBand(usage: number) {
   return usage > 200 ? usage - 200 : 0;
+}
+
+function withinBand(usage: number, bottom: number, top: number) {
+  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
 }
