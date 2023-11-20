405a697225e3e8d83a474bb2ef48fe3df9aa91d4
Tony Lee <tony@treasurer.co.kr>
2023-11-20 13:23:18 +0900

    11-2 함수 매개변수화하기 : v2 init

---
+++ chapter11/11-2/parameterizeFunctionV2.ts
@@ -0,0 +1,27 @@
+/**
+ * 대역에 따른 요금을 계산한다.
+ */
+function baseCharge(usage: number) {
+  if (usage < 0) {
+    return usd(0);
+  }
+  const amount =
+    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+  return usd(amount);
+}
+
+function usd(num: number) {
+  return num / 1000;
+}
+
+function bottomBand(usage: number) {
+  return Math.min(usage, 100);
+}
+
+function middleBand(usage: number) {
+  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+}
+
+function topBand(usage: number) {
+  return usage > 200 ? usage - 200 : 0;
+}
