dbf88bf17b6c2d72e4d6bb67a14c3fffd296391a
Tony Lee <tony@treasurer.co.kr>
2023-12-06 13:22:15 +0900

    11-4 객체 통째로 넘기기 V2 - step4-1. 매개변수 변경

--- chapter11/11-4/preserveWholeObjV2.ts
+++ chapter11/11-4/preserveWholeObjV2.ts
@@ -12,10 +12,10 @@ class HeatingPlan {
       bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
     );
   }
-  xxNEWwithinRange(aPlan, tempRange) {
+  xxNEWwithinRange(tempRange) {
     const low = tempRange.low;
     const high = tempRange.high;
-    const isWithinRange = aPlan.withinRange(low, high);
+    const isWithinRange = this.withinRange(low, high);
     return isWithinRange;
   }
 }
@@ -29,7 +29,7 @@ const aRoom = {
 };
 const tempRange = aRoom.daysTempRange;
 const aPlan = new HeatingPlan({ low: 20, high: 30 });
-const isWithinRange = aPlan.xxNEWwithinRange(aPlan, tempRange);
+const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
 if (!isWithinRange) {
   alert.push("방 온도가 지정 범위를 벗어났습니다.");
 }
