0cd6a50e6dce1177e4760d6f1e8ebabe94636b64
Tony Lee <tony@treasurer.co.kr>
2023-12-06 13:21:22 +0900

    11-4 객체 통째로 넘기기 V2 - step4. 추출한 함수 클래스 메서드로 옮기기

--- chapter11/11-4/preserveWholeObjV2.ts
+++ chapter11/11-4/preserveWholeObjV2.ts
@@ -12,6 +12,12 @@ class HeatingPlan {
       bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
     );
   }
+  xxNEWwithinRange(aPlan, tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = aPlan.withinRange(low, high);
+    return isWithinRange;
+  }
 }
 
 // 호출자
@@ -23,16 +29,9 @@ const aRoom = {
 };
 const tempRange = aRoom.daysTempRange;
 const aPlan = new HeatingPlan({ low: 20, high: 30 });
-const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+const isWithinRange = aPlan.xxNEWwithinRange(aPlan, tempRange);
 if (!isWithinRange) {
   alert.push("방 온도가 지정 범위를 벗어났습니다.");
-}
-
-function xxNEWwithinRange(aPlan, tempRange) {
-  const low = tempRange.low;
-  const high = tempRange.high;
-  const isWithinRange = aPlan.withinRange(low, high);
-  return isWithinRange;
 }
 
 export default {};
