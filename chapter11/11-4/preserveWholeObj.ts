778a9fc6035e1f327c5e89e669da985cfa54ac81
Tony Lee <tony@treasurer.co.kr>
2023-12-06 13:07:46 +0900

    11-4 객체 통째로 넘기기 : init

---
+++ chapter11/11-4/preserveWholeObj.ts
@@ -0,0 +1,30 @@
+class HeatingPlan {
+  _temperatureRange: {
+    low: number;
+    high: number;
+  };
+  constructor(temperatureRange: { low: number; high: number }) {
+    this._temperatureRange = temperatureRange;
+  }
+  withinRange(bottom: number, top: number) {
+    return (
+      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+    );
+  }
+}
+
+// 호출자
+const aRoom = {
+  daysTempRange: {
+    low: 10,
+    high: 30,
+  },
+};
+const aPlan = new HeatingPlan({ low: 20, high: 30 });
+const low = aRoom.daysTempRange.low;
+const high = aRoom.daysTempRange.high;
+if (!aPlan.withinRange(low, high)) {
+  alert.push("방 온도가 지정 범위를 벗어났습니다.");
+}
+
+export default {};
