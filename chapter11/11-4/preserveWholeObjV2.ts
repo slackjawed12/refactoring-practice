36da27c41dc73259328d47e8815ac3dfef227e58
Tony Lee <tony@treasurer.co.kr>
2023-12-06 13:16:48 +0900

    11-4 객체 통째로 넘기기 V2 - step1. 기존 메서드 호출부를 변수로 추출하기

--- chapter11/11-4/preserveWholeObjV2.ts
+++ chapter11/11-4/preserveWholeObjV2.ts
@@ -24,7 +24,8 @@ const aRoom = {
 const aPlan = new HeatingPlan({ low: 20, high: 30 });
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
-if (!aPlan.withinRange(low, high)) {
+const isWithinRange = aPlan.withinRange(low, high);
+if (!isWithinRange) {
   alert.push("방 온도가 지정 범위를 벗어났습니다.");
 }
 
