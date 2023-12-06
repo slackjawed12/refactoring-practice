49c28777653a9fb3665bb15d41714acd5226088e
Tony Lee <tony@treasurer.co.kr>
2023-12-06 13:17:40 +0900

    11-4 객체 통째로 넘기기 V2 - step2. 입력 매개변수 추출

--- chapter11/11-4/preserveWholeObjV2.ts
+++ chapter11/11-4/preserveWholeObjV2.ts
@@ -21,9 +21,10 @@ const aRoom = {
     high: 30,
   },
 };
+const tempRange = aRoom.daysTempRange;
 const aPlan = new HeatingPlan({ low: 20, high: 30 });
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
+const low = tempRange.low;
+const high = tempRange.high;
 const isWithinRange = aPlan.withinRange(low, high);
 if (!isWithinRange) {
   alert.push("방 온도가 지정 범위를 벗어났습니다.");
