6696ea769a14cb9d89b0fc73a8c05fe6908447a0
Tony Lee <tony@treasurer.co.kr>
2023-12-06 13:19:59 +0900

    11-4 객체 통째로 넘기기 V2 - step3. 추출한 매개변수 받도록 함수 추출

--- chapter11/11-4/preserveWholeObjV2.ts
+++ chapter11/11-4/preserveWholeObjV2.ts
@@ -23,11 +23,16 @@ const aRoom = {
 };
 const tempRange = aRoom.daysTempRange;
 const aPlan = new HeatingPlan({ low: 20, high: 30 });
-const low = tempRange.low;
-const high = tempRange.high;
-const isWithinRange = aPlan.withinRange(low, high);
+const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
 if (!isWithinRange) {
   alert.push("방 온도가 지정 범위를 벗어났습니다.");
+}
+
+function xxNEWwithinRange(aPlan, tempRange) {
+  const low = tempRange.low;
+  const high = tempRange.high;
+  const isWithinRange = aPlan.withinRange(low, high);
+  return isWithinRange;
 }
 
 export default {};
