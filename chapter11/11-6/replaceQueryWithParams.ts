7ae8344e803ce049c772d4ca80d91d75b8c18fa2
Tony Lee <tony@treasurer.co.kr>
2023-12-07 13:36:31 +0900

    11-6 질의함수를 매개변수로 바꾸기 : init

---
+++ chapter11/11-6/replaceQueryWithParams.ts
@@ -0,0 +1,35 @@
+const thermostat = {
+  selectedTemperature: 100,
+  currentTemperature: 55,
+};
+
+class HeatingPlan {
+  _max: number;
+  _min: number;
+  constructor(max: number, min: number) {
+    this._max = max;
+    this._min = min;
+  }
+  get targetTemperature() {
+    if (thermostat.selectedTemperature > this._max) {
+      return this._max;
+    } else if (thermostat.selectedTemperature < this._min) {
+      return this._min;
+    } else {
+      return thermostat.selectedTemperature;
+    }
+  }
+}
+
+// 호출자
+const thePlan = new HeatingPlan(50, 10);
+const setToHeat = () => {};
+const setToCool = () => {};
+const setOff = () => {};
+if (thePlan.targetTemperature > thermostat.currentTemperature) {
+  setToHeat();
+} else if (thePlan.targetTemperature < thermostat.currentTemperature) {
+  setToCool();
+} else {
+  setOff();
+}
