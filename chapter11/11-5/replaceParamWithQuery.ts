d525827050fab0a6d7ad1c98867e9ef9be69cb6f
Tony Lee <tony@treasurer.co.kr>
2023-12-07 13:13:44 +0900

    11-5 매개변수를 질의함수로 바꾸기 : init

---
+++ chapter11/11-5/replaceParamWithQuery.ts
@@ -0,0 +1,30 @@
+class Order {
+  quantity: number;
+  itemPrice: number;
+  constructor(quantity: number, itemPrice: number) {
+    this.quantity = quantity;
+    this.itemPrice = itemPrice;
+  }
+
+  get finalPrice() {
+    const basePrice = this.quantity * this.itemPrice;
+    let discountLevel;
+    if (this.quantity > 100) {
+      discountLevel = 2;
+    } else {
+      discountLevel = 1;
+    }
+    return this.discountPrice(basePrice, discountLevel);
+  }
+
+  discountPrice(basePrice: number, discountLevel: number) {
+    switch (discountLevel) {
+      case 1:
+        return basePrice * 0.95;
+      case 2:
+        return basePrice * 0.9;
+    }
+  }
+}
+
+export default {};
