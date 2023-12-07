54ab741c6e2e86db97d19d00770cf92c19bc49c7
Tony Lee <tony@treasurer.co.kr>
2023-12-07 13:21:00 +0900

    11-5 매개변수를 질의함수로 바꾸기 : step3. 함수 선언바꾸기(6-5)로 기존함수 대체

--- chapter11/11-5/replaceParamWithQuery.ts
+++ chapter11/11-5/replaceParamWithQuery.ts
@@ -8,14 +8,14 @@ class Order {
 
   get finalPrice() {
     const basePrice = this.quantity * this.itemPrice;
-    return this.discountPrice(basePrice, this.discountLevel);
+    return this.discountPrice(basePrice);
   }
 
   get discountLevel() {
     return this.quantity > 100 ? 2 : 1;
   }
 
-  discountPrice(basePrice: number, discountLevel: number) {
+  discountPrice(basePrice: number) {
     switch (this.discountLevel) {
       case 1:
         return basePrice * 0.95;
