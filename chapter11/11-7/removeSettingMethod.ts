f7415680190a3d4d1e102941eb122575e9ae3fcc
Tony Lee <tony@treasurer.co.kr>
2023-12-19 13:37:23 +0900

    11-7 세터 제거하기 : init

---
+++ chapter11/11-7/removeSettingMethod.ts
@@ -0,0 +1,26 @@
+// 리팩터링 필요한 상황
+// 1. 무조건 getter를 통해서만 필드를 다뤄야 할 때
+// 2. 생성자 호출 후 일련의 setter를 호출해서 객체를 완성하는 스크립트 코드가 있을 때
+// setter 제거해서 의도를 명확히 밝히는 것이 더 바람직하다.
+
+class Person {
+  _name: string;
+  _id: string;
+  constructor() {}
+  get name() {
+    return this._name;
+  }
+  set name(arg) {
+    this._name = arg;
+  }
+  get id() {
+    return this._id;
+  }
+  set id(arg) {
+    this._id = arg;
+  }
+}
+
+const martin = new Person();
+martin.name = "마틴";
+martin.id = "1234";
