db3af8eb345a463d0a808afbb6172c313088405b
Tony Lee <tony@treasurer.co.kr>
2023-12-21 13:42:54 +0900

    11-9 함수를 명령으로 바꾸기 : step1, step2 - 빈 클래스 작성 및 함수를 클래스로 옮기기

--- chapter11/11-9/replaceFunctionWithCommand.ts
+++ chapter11/11-9/replaceFunctionWithCommand.ts
@@ -1,21 +1,27 @@
 // 점수 계산 함수
 function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+  return new Scorer().execute(candidate, medicalExam, scoringGuide);
+}
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+class Scorer {
+  execute(candidate, medicalExam, scoringGuide) {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+
+    let certificationGrade = "regular";
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+      certificationGrade = "low";
+      result -= 5;
+    }
+    // ....
+
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-
-  let certificationGrade = "regular";
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = "low";
-    result -= 5;
-  }
-  // ....
-
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
