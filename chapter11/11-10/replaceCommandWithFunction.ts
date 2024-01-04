f09a076ebac4a0e7a9a96ef3ddaa2839cc094de4
Tony Lee <tony@treasurer.co.kr>
2024-01-04 13:25:12 +0900

    11-10 명령을 함수로 바꾸기 : step5. 실행 메서드를 추출한 함수로 인라인

--- chapter11/11-10/replaceCommandWithFunction.ts
+++ chapter11/11-10/replaceCommandWithFunction.ts
@@ -21,9 +21,6 @@ let monthCharge = charge(customer, usage, provider);
 // ...
 
 function charge(customer, usage, provider) {
-  return new ChargeCalculator(customer, usage, provider).charge(
-    customer,
-    usage,
-    provider
-  );
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
