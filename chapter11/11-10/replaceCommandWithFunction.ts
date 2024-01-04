dffb13d617d1138b5965135ecf871b975ea57882
Tony Lee <tony@treasurer.co.kr>
2024-01-04 13:24:13 +0900

    11-10 명령을 함수로 바꾸기 : step4. 필드 대신 매개변수를 사용하도록 변경

--- chapter11/11-10/replaceCommandWithFunction.ts
+++ chapter11/11-10/replaceCommandWithFunction.ts
@@ -1,21 +1,10 @@
 // 명령을 함수로 바꾸기 - 명령 메서드의 복잡성이 크지 않으면 단일 함수로 추출하는 것이 낫다.
 class ChargeCalculator {
-  _customer;
-  _usage;
-  _provider;
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
+  constructor(customer, usage, provider) {}
 
   charge(customer, usage, provider) {
-    const baseCharge = this._customer.baseRate * this._usage;
-    return baseCharge + this._provider.connectionCharge;
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
   }
 }
 
