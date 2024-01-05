5aeda098d992d871b6b13545302c884c03103402
Tony Lee <tony@treasurer.co.kr>
2024-01-05 13:25:08 +0900

    11-11. 수정된 값 반환하기 - init

---
+++ chapter11/11-11/returnModifiedValue.ts
@@ -0,0 +1,24 @@
+const points: { elevation: number }[] = [];
+let totalAscent = 0;
+let totalTime = 0;
+let totalDistance = 0;
+calculateAscent();
+calculateTime();
+calculateDistance();
+
+const pace = totalTime / 60 / totalDistance;
+
+function calculateAscent() {
+  for (let i = 0; i < points.length; i++) {
+    const verticalChange = points[i].elevation - points[i - 1].elevation;
+    totalAscent += verticalChange > 0 ? verticalChange : 0;
+  }
+}
+
+function calculateTime() {
+  // ...
+}
+
+function calculateDistance() {
+  // ...
+}
