f5f23f920d4ae3d320bdc3c444cf7b84d2f8e7c5
Tony Lee <tony@treasurer.co.kr>
2024-01-08 13:29:24 +0900

    11-12 오류코드를 예외로 바꾸기: init - 호출자

--- chapter11/11-12/replaceErrorCodeWithException.ts
+++ chapter11/11-12/replaceErrorCodeWithException.ts
@@ -1,6 +1,15 @@
 class ShippingRules {
   constructor(data) {}
 }
+class Order {
+  _country;
+  constructor(country: string) {
+    this._country = country;
+  }
+  get country() {
+    return this._country;
+  }
+}
 
 const countryData = {
   shippingRules: {
@@ -8,6 +17,30 @@ const countryData = {
     KOR: {},
   },
 };
+
+let errorList: any[] = [];
+const orderData = new Order("1234");
+
+// 상위 호출자
+const status = calculateShippingCosts(orderData);
+if (status < 0) {
+  errorList.push({
+    order: orderData,
+    errorCode: status,
+  });
+}
+
+// 호출자
+function calculateShippingCosts(anOrder: Order) {
+  // 관련 없는 코드
+  const shippingRules = localShippingRules(anOrder.country);
+  if (shippingRules < 0) {
+    // 오류 전파
+    return shippingRules;
+  }
+
+  // 더 관련 없는 코드
+}
 
 function localShippingRules(country: string) {
   const data = countryData.shippingRules[country];
