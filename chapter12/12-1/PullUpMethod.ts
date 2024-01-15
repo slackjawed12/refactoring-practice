c51cb1294fbadc9e3f311ec441d4c50fddd268a2
Tony Lee <tony@treasurer.co.kr>
2024-01-15 13:34:44 +0900

    12-1 메서드 올리기 : step1. init - 같은 일을 하는 메서드 찾기

---
+++ chapter12/12-1/PullUpMethod.ts
@@ -0,0 +1,25 @@
+class Party {}
+
+class Employee extends Party {
+  monthlyCost: number;
+  constructor(monthlyCost: number) {
+    super();
+    this.monthlyCost = monthlyCost;
+  }
+
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department extends Party {
+  monthlyCost: number;
+  constructor(monthlyCost: number) {
+    super();
+    this.monthlyCost = monthlyCost;
+  }
+
+  get totalAnnualCost() {
+    return this.monthlyCost * 12;
+  }
+}
