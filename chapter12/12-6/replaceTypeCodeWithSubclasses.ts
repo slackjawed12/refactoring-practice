05d5b949080646670fe64941176b79722578b557
Tony Lee <tony@treasurer.co.kr>
2024-01-18 13:45:41 +0900

    12-6 타입 코드를 서브클래스로 바꾸기: step3-1 새로 만든 서브클래스 선택 로직을 팩터리에 추가

--- chapter12/12-6/replaceTypeCodeWithSubclasses.ts
+++ chapter12/12-6/replaceTypeCodeWithSubclasses.ts
@@ -30,5 +30,10 @@ class Engineer extends Employee {
 }
 
 function createEmployee(name, type) {
+  switch (type) {
+    case "engineer":
+      return new Engineer(name, type);
+  }
+
   return new Employee(name, type);
 }
