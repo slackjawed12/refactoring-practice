5349da4f31b746d581016d0e2fd5c8a052992f1d
Tony Lee <tony@treasurer.co.kr>
2024-01-18 13:41:14 +0900

    12-6 타입 코드를 서브클래스로 바꾸기: init

---
+++ chapter12/12-6/replaceTypeCodeWithSubclasses.ts
@@ -0,0 +1,18 @@
+class Employee {
+  _name: string;
+  _type: string;
+  constructor(name, type) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+
+  validateType(arg) {
+    if (!["engineer", "manager", "salesperson"].includes(arg)) {
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
+  toString() {
+    return `${this._name} (${this._type})`;
+  }
+}
