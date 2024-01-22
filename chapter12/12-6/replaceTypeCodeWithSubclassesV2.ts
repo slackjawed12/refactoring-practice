7fb7a90105ca5b704dde173cc9c0d94321c804c2
Tony Lee <tony@treasurer.co.kr>
2024-01-22 13:27:38 +0900

    12-6 타입 코드를 서브클래스로 바꾸기 간접상속 경우 - init

---
+++ chapter12/12-6/replaceTypeCodeWithSubclassesV2.ts
@@ -0,0 +1,34 @@
+// 간접 상속하는 경우
+class Employee {
+  _name: string;
+  _type: string;
+  constructor(name: string, type: string) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+
+  validateType(arg: string) {
+    if (!["engineer", "manager", "salesperson"].includes(arg)) {
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  set type(arg: string) {
+    this._type = arg;
+  }
+
+  get capitalizedType() {
+    return (
+      this._type.charAt(0).toUpperCase() + this._type.substring(1).toLowerCase()
+    );
+  }
+
+  toString() {
+    return `${this._name} (${this.capitalizedType})`;
+  }
+}
