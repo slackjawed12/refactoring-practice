af14b2158d103adc47cf4e1bd64f3881a5ccdf9a
Tony Lee <tony@treasurer.co.kr>
2024-01-23 13:34:38 +0900

    12-7 서브클래스 제거하기 - init

---
+++ chapter12/12-7/removeSubclass.ts
@@ -0,0 +1,28 @@
+class Person {
+  _name: string;
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get genderCode() {
+    return "X";
+  }
+}
+
+class Male extends Person {
+  get genderCode() {
+    return "M";
+  }
+}
+
+class Female extends Person {
+  get genderCode() {
+    return "F";
+  }
+}
+
+export default {};
