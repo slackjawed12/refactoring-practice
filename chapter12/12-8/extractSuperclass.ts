ce702fb4c1963d162f14c8d7c688625d93df4aa5
Tony Lee <tony@treasurer.co.kr>
2024-01-24 13:31:15 +0900

    12-8 슈퍼클래스 추출하기 - init

---
+++ chapter12/12-8/extractSuperclass.ts
@@ -0,0 +1,58 @@
+class Employee {
+  _id;
+  _name;
+  _monthlyCost;
+  constructor(name, id, monthlyCost) {
+    this._id = id;
+    this._name = name;
+    this._monthlyCost = monthlyCost;
+  }
+  get monthlyCost() {
+    return this._monthlyCost;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department {
+  _name;
+  _staff;
+  constructor(name, staff) {
+    this._name = name;
+    this._staff = staff;
+  }
+
+  get staff() {
+    return this._staff.slice();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get totalMonthlyCost() {
+    return this.staff
+      .map((e) => e.monthlyCost)
+      .reduce((sum, cost) => sum + cost);
+  }
+
+  get headCount() {
+    return this.staff.length;
+  }
+
+  get totalAnnualCost() {
+    return this.totalMonthlyCost * 12;
+  }
+}
+
+export default {};
