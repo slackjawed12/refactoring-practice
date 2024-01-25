0549b6307e15b1a0642ea9d97107db9ec25d4692
Tony Lee <tony@treasurer.co.kr>
2024-01-25 13:10:41 +0900

    12-8 슈퍼클래스 추출하기 - step2-3. annualCost 메서드 슈퍼클래스로 옮기기 +

--- chapter12/12-8/extractSuperclass.ts
+++ chapter12/12-8/extractSuperclass.ts
@@ -1,22 +1,31 @@
 class Party {
-  _name;
-  constructor(name) {
+  _name: string;
+  constructor(name: string) {
     this._name = name;
   }
 
   get name() {
     return this._name;
   }
+
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+
+  get monthlyCost() {
+    return 0;
+  }
 }
 
 class Employee extends Party {
-  _id;
-  _monthlyCost;
-  constructor(name, id, monthlyCost) {
+  _id: number;
+  _monthlyCost: number;
+  constructor(name: string, id: number, monthlyCost: number) {
     super(name);
     this._id = id;
     this._monthlyCost = monthlyCost;
   }
+
   get monthlyCost() {
     return this._monthlyCost;
   }
@@ -24,15 +33,11 @@ class Employee extends Party {
   get id() {
     return this._id;
   }
-
-  get annualCost() {
-    return this.monthlyCost * 12;
-  }
 }
 
 class Department extends Party {
-  _staff;
-  constructor(name, staff) {
+  _staff: Employee[];
+  constructor(name: string, staff: Employee[]) {
     super(name);
     this._staff = staff;
   }
@@ -50,10 +55,9 @@ class Department extends Party {
   get headCount() {
     return this.staff.length;
   }
-
-  get annualCost() {
-    return this.monthlyCost * 12;
-  }
 }
 
 export default {};
+
+const employee = new Employee("홍길동", 1, 10000);
+console.log(employee.annualCost);
