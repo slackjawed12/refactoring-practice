112515be09f894907da9ddf0af1986cc0908bcab
Tony Lee <tony@treasurer.co.kr>
2024-02-01 13:27:14 +0900

    12-10 여러 서브클래스를 위임으로 바꾸기 - step12. 서브클래스 제거

--- chapter12/12-10/replaceSubclassesWithDelegateV2.ts
+++ chapter12/12-10/replaceSubclassesWithDelegateV2.ts
@@ -44,18 +44,6 @@ class Bird {
   }
 }
 
-class AfricanSwallow extends Bird {
-  _numberOfCoconuts: number;
-  constructor(data: BirdData) {
-    super(data);
-    this._numberOfCoconuts = data.numberOfCoconuts;
-  }
-
-  get airSpeedVelocity() {
-    return this._speciesDelegate.airSpeedVelocity;
-  }
-}
-
 class NorwegianBlueParrot extends Bird {
   _voltage: number;
   _isNailed: boolean;
@@ -97,8 +85,6 @@ class AfricanSwallowDelegate {
 
 function createBird(data: BirdData) {
   switch (data.type) {
-    case "아프리카 제비":
-      return new AfricanSwallow(data);
     case "노르웨이 파랑 앵무":
       return new NorwegianBlueParrot(data);
     default:
