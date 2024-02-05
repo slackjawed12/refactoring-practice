0698f17476c18bf5aedf818b4b8fd341a10d684c
Tony Lee <tony@treasurer.co.kr>
2024-02-05 13:41:54 +0900

    12-11 슈퍼클래스를 위임으로 바꾸기 - init

---
+++ chapter12/12-11/replaceSuperclassWithDelegate.ts
@@ -0,0 +1,45 @@
+export class CatalogItem {
+  _id: number;
+  _title: string;
+  _tags: string[];
+  constructor(id: number, title: string, tags: string[]) {
+    this._id = id;
+    this._title = title;
+    this._tags = tags;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  hasTag(arg: string) {
+    return this._tags.includes(arg);
+  }
+}
+
+export class Scroll extends CatalogItem {
+  _lastCleaned;
+  constructor(id: number, title: string, tag: string[], dateLastCleaned) {
+    super(id, title, tag);
+    this._lastCleaned = dateLastCleaned;
+  }
+
+  needsCleaning(targetDate) {
+    const threshold = this.hasTag("revered") ? 700 : 1500;
+    return this.daysSinceLastCleaning(targetDate) > threshold;
+  }
+
+  daysSinceLastCleaning(targetDate) {
+    return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
+  }
+}
+
+class ChronoUnit {
+  static DAYS;
+}
+
+export default {};
